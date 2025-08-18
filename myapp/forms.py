ba9cbc91cab1d8ed300d7f7037c6914ef49b7aa8
Mauro fassi <maurofassi17@gmail.com>
2025-08-18 08:22:36 -0300

    Modificaciones varias

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,11 +1,22 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Task
+from .models import Task, Project
 
 class CreateNewTask(forms.Form):
-    title=forms.CharField(label="Titulo de la tarea")
-    description=forms.CharField(label="Descripcion de la tarea",widget=forms.Textarea)
-
+    title = forms.CharField(
+        label="Titulo de la tarea",
+        # Aquí se añade el widget y la clase form-control
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    description = forms.CharField(
+        label="Descripcion de la tarea",
+        widget=forms.Textarea(attrs={'class': 'form-control'})
+    )
+    proyecto = forms.ModelChoiceField(
+        label="Proyecto al que pertenece",
+        queryset=Project.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
 
 class editar_tarea(ModelForm):
     class Meta:
